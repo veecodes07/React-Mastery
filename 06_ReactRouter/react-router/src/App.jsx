3a4fe5e8f42d5bbdaa2b60c0daa25828b232a27e
vee <95954865+veecodes07@users.noreply.github.com>
2023-12-11 07:15:08 +0000

    react router dom project

--- 06_ReactRouter/react-router/src/App.jsx
+++ 06_ReactRouter/react-router/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <h1 className='bg-green-600 p-4 '>React Router </h1>
+      
     </>
   )
 }
--- 06_ReactRouter/react-router/src/main.jsx
+++ 06_ReactRouter/react-router/src/main.jsx
@@ -2,9 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Layout from './Layout.jsx'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout/>,
+    children: [{
+      path: "",
+      element: <Home/>
+    }, 
+    {}]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <RouterProvider/> //needs props to function */}
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
