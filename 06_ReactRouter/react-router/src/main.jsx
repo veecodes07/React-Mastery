5db908b1b45e64792ff713853f1bf7fbc7d238ec
vee <95954865+veecodes07@users.noreply.github.com>
2023-12-12 10:41:02 +0000

    github edit

--- 06_ReactRouter/react-router/src/main.jsx
+++ 06_ReactRouter/react-router/src/main.jsx
@@ -8,6 +8,7 @@ import About from './components/About/About.jsx'
 import Home from './components/Home/Home.jsx'
 import Contact from './components/Contact/Contact.jsx'
 import User from './components/User/User.jsx'
+import Github from './components/Github/Github.jsx'
 
 
 const router = createBrowserRouter([
@@ -29,6 +30,10 @@ const router = createBrowserRouter([
     {
       path: "user/:userid",
       element: <User />
+    },
+    {
+      path: "github",
+      element: <Github />
     }
   ]
   }
