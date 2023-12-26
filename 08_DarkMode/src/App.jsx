f30be8cb6226f9e504234c16baa09938ea4391b1
vee <95954865+veecodes07@users.noreply.github.com>
2023-12-26 11:00:55 +0000

    darkmode completed

--- 08_DarkMode/src/App.jsx
+++ 08_DarkMode/src/App.jsx
@@ -1,7 +1,9 @@
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import { ThemeProvider } from './context/theme'
+import ThemeBtn from './components/ThemeBtn'
+import Card from './components/Card'
 
 function App() {
 
@@ -15,16 +17,25 @@ function App() {
     setThemeMode("dark")
   }
   
+
+  //ACTUAL FUNCTIONALITY 
+   useEffect(() => {
+    document.querySelector('html').classList.remove("light", "dark")
+    document.querySelector('html').classList.add(themeMode)
+   }, [themeMode])
+
+
+
   return (
     <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
 <div className="flex flex-wrap min-h-screen items-center">
                 <div className="w-full">
                     <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-                        {/* theme button */}
+                        <ThemeBtn />
                     </div>
 
                     <div className="w-full max-w-sm mx-auto">
-                       {/* card */}
+                       <Card />
                     </div>
                 </div>
             </div>
