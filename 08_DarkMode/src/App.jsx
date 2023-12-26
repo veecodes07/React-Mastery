b7931146fe4af6fa206f942b584fa94f44a44b38
vee <95954865+veecodes07@users.noreply.github.com>
2023-12-26 10:22:50 +0000

    made hooks and provider

--- 08_DarkMode/src/App.jsx
+++ 08_DarkMode/src/App.jsx
@@ -1,10 +1,11 @@
 
 import './App.css'
+import { ThemeProvider } from './context/theme'
 
 function App() {
   
   return (
-    
+    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
 <div className="flex flex-wrap min-h-screen items-center">
                 <div className="w-full">
                     <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
@@ -16,6 +17,7 @@ function App() {
                     </div>
                 </div>
             </div>
+            </ThemeProvider>
 
   )
 }
