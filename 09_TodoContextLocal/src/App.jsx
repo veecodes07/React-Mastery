9f687631b0b0d710a894564ec3cef0bcc2ccc54e
vee <95954865+veecodes07@users.noreply.github.com>
2024-01-01 11:09:14 +0000

    todo item

--- 09_TodoContextLocal/src/App.jsx
+++ 09_TodoContextLocal/src/App.jsx
@@ -1,11 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState , useEffect} from 'react'
+
 import './App.css'
 import { TodoProvider } from './contexts'
 
 function App() {
   const [todos, setTodos] = useState([])
+
+  const addTodo = (todo) => {
+    // destructured the old values in todo using ...
+    setTodos((prev) => [{id: Date.now(), ...todo}, ...prev]) 
+
+  }
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id=== id ? todo : prevTodo)))
+  }
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id))
+  }
+
+  const toggleComplete = (id) => {
+    setTodos((prev) => prev.map((prevTodo) => prevTodo === id ? 
+    {...prevTodo, completed: !prevTodo.completed} : prevTodo))
+  } 
+
+  useEffect(() => {
+  const todos =   JSON.parse(localStorage.getItem("todos")
+    )
+    if (todos && todos.length > 0) {
+      setTodos(todos)
+      
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
+  
 
   return (
     <TodoProvider value={{todos, addTodo, updateTodo,deleteTodo,toggleComplete}}>
