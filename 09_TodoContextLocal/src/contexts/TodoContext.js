8cf8f859118bf8fd50b1aab7708438234a44dcb0
vee <95954865+veecodes07@users.noreply.github.com>
2024-01-01 10:35:24 +0000

    todo app

--- 09_TodoContextLocal/src/contexts/TodoContext.js
+++ 09_TodoContextLocal/src/contexts/TodoContext.js
@@ -4,10 +4,23 @@ import { createContext, useContext } from "react";
 //create and export a context to use the created context
 //create and export the context to provide the context in other files with .provider
 
-export const TodoContext = createContext({})
+export const TodoContext = createContext({
+    todo: [
+        {
+            id: 1,
+            todo: 'todo msg',
+            completed: false
+        }
+    ],
+    addTodo: (todo) => { }, //Just need to define the functions need to perform
+    //NO NEED TO TO DESCRIBE FUNCTIONALITY HERE, DONE IN APP.JSX
+    updateTodo: (id, todo) => {},
+    deleteTodo: (id) => {},
+    toggleComplete: (id) => {}
+})
 
 export const useTodo = () => {
     return useContext(TodoContext)
 }
 
-export const Todoprovider = TodoContext.Provider
+export const TodoProvider = TodoContext.Provider
