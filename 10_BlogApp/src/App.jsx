56b52f899530fa3e20d32ffd4b2648b89a6a11b2
vee <95954865+veecodes07@users.noreply.github.com>
2024-01-08 10:53:25 +0000

    Blog App

---
+++ 10_BlogApp/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  console.log(process.env.REACT_APP_APPWRITE_URL);
+  
+
+  return (
+    <>
+      <h1> A  BLOG WITH APPWRITE </h1>
+    </>
+  )
+}
+
+export default App
